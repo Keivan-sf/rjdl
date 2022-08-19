f743ae4ea0c25c22077549f5e9f566a9fe937649
Keivan <keyvan0082@gmail.com>
2022-08-20 04:04:00 +0430

    Configure `prettier`

--- lib/utils/urlUtils/urlUtilities.test.ts
+++ lib/utils/urlUtils/urlUtilities.test.ts
@@ -17,9 +17,7 @@ describe("Radio javan link validation", () => {
         );
     });
     test("Should get the core url without prefix", () => {
-        expect(getRadioJavanLink("radiojavan.com/")).toBe(
-            "radiojavan.com/"
-        );
+        expect(getRadioJavanLink("radiojavan.com/")).toBe("radiojavan.com/");
     });
     test("Should get a random url with the origin of RadioJavan", () => {
         expect(getRadioJavanLink("https://www.radiojavan.com/random")).toBe(
