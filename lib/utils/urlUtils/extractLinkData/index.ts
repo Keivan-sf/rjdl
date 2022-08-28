231216991f8979f49fe14fa775bc0c4bc1019998
Keivan <keyvan0082@gmail.com>
2022-08-28 21:31:25 +0430

    Get name and id from URLs

--- lib/utils/urlUtils/extractLinkData/index.ts
+++ lib/utils/urlUtils/extractLinkData/index.ts
@@ -25,3 +25,38 @@ export const getMusicNameFromURL = (url: string): string => {
     name = name.includes("?") ? name.split("?")[0] : name;
     return name;
 };
+
+export const getVideoNameFromURL = (url: string): string => {
+    let name = url.split("/videos/video/")[1];
+    name = name.includes("/") ? name.split("/")[0] : name;
+    name = name.includes("?") ? name.split("?")[0] : name;
+    return name;
+};
+
+export const getAlbumNameFromURL = (url: string): string => {
+    let name = url.split("/mp3s/album/")[1];
+    name = name.includes("/") ? name.split("/")[0] : name;
+    name = name.includes("?") ? name.split("?")[0] : name;
+    return name;
+};
+
+export const getIdFromPlaylistURL = (url: string): string => {
+    let name = url.split("/playlists/playlist/mp3/")[1];
+    name = name.includes("/") ? name.split("/")[0] : name;
+    name = name.includes("?") ? name.split("?")[0] : name;
+    return name;
+};
+
+export const getIDFromPlaylistTrackURL = (url: string): string => {
+    let name = url.split("/mp3s/playlist_start?id=")[1];
+    name = name.includes("/") ? name.split("/")[0] : name;
+    name = name.includes("&") ? name.split("&")[0] : name;
+    return name;
+};
+
+export const getPodcastNameFromURL = (url: string): string => {
+    let name = url.split("/podcasts/podcast/")[1];
+    name = name.includes("/") ? name.split("/")[0] : name;
+    name = name.includes("?") ? name.split("?")[0] : name;
+    return name;
+};
--- lib/utils/urlUtils/urlUtilities.test.ts
+++ lib/utils/urlUtils/urlUtilities.test.ts
@@ -4,6 +4,11 @@ import {
     getRadioJavanLink,
     getTypeFromValidURL,
     getMusicNameFromURL,
+    getVideoNameFromURL,
+    getAlbumNameFromURL,
+    getIdFromPlaylistURL,
+    getIDFromPlaylistTrackURL,
+    getPodcastNameFromURL,
 } from "./extractLinkData";
 
 describe("Radio javan url utils", () => {
@@ -188,6 +193,48 @@ describe("Radio javan url utils", () => {
                 "https://www.radiojavan.com/mp3s/mp3/Sogand-Baroon?start=109320&index=2"
             );
             expect(name).toBe("Sogand-Baroon");
+        });
+        test("Should return the video name", () => {
+            const name = getVideoNameFromURL(
+                "https://www.radiojavan.com/videos/video/donya-bye-bye-bye"
+            );
+            expect(name).toBe("donya-bye-bye-bye");
+        });
+        test("should return the album name", () => {
+            const name = getAlbumNameFromURL(
+                "https://www.radiojavan.com/mp3s/album/Koorosh-420"
+            );
+            expect(name).toBe("Koorosh-420");
+        });
+        test("Should return the album name from albumTrack", () => {
+            const name = getAlbumNameFromURL(
+                "https://www.radiojavan.com/mp3s/album/Koorosh-420?index=3"
+            );
+            expect(name).toBe("Koorosh-420");
+        });
+        test("Should return the playlistID from playlist url", () => {
+            const name = getIdFromPlaylistURL(
+                "https://www.radiojavan.com/playlists/playlist/mp3/1249011caf74"
+            );
+            expect(name).toBe("1249011caf74");
+        });
+        test("Should return the playlistID from playlistTrack url", () => {
+            const name = getIDFromPlaylistTrackURL(
+                "https://www.radiojavan.com/mp3s/playlist_start?id=1249011caf74&index=2"
+            );
+            expect(name).toBe("1249011caf74");
+        });
+        test("Should return the podcast name from url", () => {
+            const name = getPodcastNameFromURL(
+                "https://www.radiojavan.com/podcasts/podcast/Dubways-133"
+            );
+            expect(name).toBe("Dubways-133");
+        });
+        test("Should return the podcast name from related PodcastUrl", () => {
+            const name = getPodcastNameFromURL(
+                "https://www.radiojavan.com/podcasts/podcast/Dubways-129?start=3594&index=4"
+            );
+            expect(name).toBe("Dubways-129");
         });
     });
 });
