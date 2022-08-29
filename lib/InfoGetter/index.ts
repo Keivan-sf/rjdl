254b663242a1d259128591a0176fe110df2aef70
Keivan <keyvan0082@gmail.com>
2022-08-29 18:44:08 +0430

    Return `MusicInfo` interface

--- lib/InfoGetter/index.ts
+++ lib/InfoGetter/index.ts
@@ -1,7 +1,8 @@
 import { getMusicInfoFromDOM } from "./getMusicInfo";
+import { MusicInfo } from "./interfaces";
 import { getSourceCodeDOMDocument } from "./utils";
 
-export const getMusicInfo = async (url: string) => {
+export const getMusicInfo = async (url: string): Promise<MusicInfo> => {
     const document = await getSourceCodeDOMDocument(url);
     const info = getMusicInfoFromDOM(document);
     const infoAndUrl = {
---
+++ lib/InfoGetter/interfaces.ts
@@ -0,0 +1,8 @@
+export interface MusicInfo {
+    name: string;
+    artist: string;
+    likes: number;
+    plays: number;
+    date: Date;
+    url: string;
+}
