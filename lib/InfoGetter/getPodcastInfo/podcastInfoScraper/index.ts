f077e3062dc0a761d73898bafd6f8cff1c20ccfa
Keivan <keyvan0082@gmail.com>
2022-09-02 18:31:03 +0430

    Add `PodcastInfoScraper`

---
+++ lib/InfoGetter/getPodcastInfo/podcastInfoScraper/index.ts
@@ -0,0 +1,30 @@
+import { BaseInfoScraper } from "../../utils";
+
+class PodcastInfoScraper extends BaseInfoScraper {
+    constructor(document: Document) {
+        super(document);
+    }
+
+    public getId = (): string => {
+        const artworkURL = this.getArtwork();
+        const id = this.getIdFromArtworkURL(artworkURL);
+        return id;
+    };
+
+    public getDate = (): Date =>
+        new Date(
+            this.document
+                .querySelector(".date_added")!
+                .innerHTML.split("Date added: ")[1]
+        );
+
+    public getArtwork = (): string =>
+        this.document.querySelector(".artwork img")!.getAttribute("src")!;
+
+    private getIdFromArtworkURL = (artworkURL: string): string => {
+        const idSelectorRegex = /(?<=static\/podcasts\/)[^\/]+(?=\/)/g;
+        return artworkURL.match(idSelectorRegex)![0];
+    };
+}
+
+export default PodcastInfoScraper;
