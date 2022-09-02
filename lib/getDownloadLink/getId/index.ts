5b28252730532f7f1b96187718f92f51917bcf49
Keivan <keyvan0082@gmail.com>
2022-09-02 21:11:27 +0430

    Get podcast ID

--- lib/getDownloadLink/getId/index.ts
+++ lib/getDownloadLink/getId/index.ts
@@ -1,5 +1,9 @@
 import { getMusicInfo } from "../../InfoGetter";
-import { getMusicIdFromURL, getVideoIdFromURL } from "../../utils/urlUtils";
+import {
+    getMusicIdFromURL,
+    getPodcastIdFromURL,
+    getVideoIdFromURL,
+} from "../../utils/urlUtils";
 import { getExtendedTypeFromValidURL } from "../../utils/urlUtils/getUrlType";
 import { ExtendedLinkType } from "../../utils/urlUtils/interfaces";
 
@@ -16,6 +20,12 @@ export const getVideoID = (url: string): string => {
     return getVideoIdFromURL(url);
 };
 
+export const getPodcastID = (url: string): string => {
+    const type = getExtendedTypeFromValidURL(url);
+    throwOnNonPodcastTypes(type);
+    return getPodcastIdFromURL(url);
+};
+
 function throwOnNonMusicTypes(type: ExtendedLinkType) {
     const allowedTypes: ExtendedLinkType[] = [
         "PlaylistTrack",
@@ -27,4 +37,8 @@ function throwOnNonMusicTypes(type: ExtendedLinkType) {
 
 function throwOnNonVideoTypes(type: ExtendedLinkType) {
     if (type !== "Video") throw new Error("NOT_A_VIDEO_LINK");
+}
+
+function throwOnNonPodcastTypes(type: ExtendedLinkType) {
+    if (type !== "Podcast") throw new Error("NOT_A_PODCAST_LINK");
 }
