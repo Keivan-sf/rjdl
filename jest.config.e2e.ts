ffeb80a5cf571bc945071c47225ee2f45d7a0b0a
Keivan <keyvan0082@gmail.com>
2022-09-03 20:17:14 +0430

    Ignore `dist` folder while testing

--- jest.config.e2e.ts
+++ jest.config.e2e.ts
@@ -1,5 +1,6 @@
 import { Config } from "jest";
 const config: Config = {
+    testPathIgnorePatterns: ["/node_modules/", "/dist/"],
     testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(test.e2e).[jt]s?(x)"],
 };
 
--- jest.config.unit.ts
+++ jest.config.unit.ts
@@ -1,5 +1,6 @@
 import { Config } from "jest";
 const config: Config = {
+    testPathIgnorePatterns: ["/node_modules/", "/dist/"],
     testMatch: [
         "**/__tests__/**/*.[jt]s?(x)",
         "**/?(*.)+(spec|test).[jt]s?(x)",
