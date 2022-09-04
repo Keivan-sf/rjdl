9e303fa56e0d72319a3c71026889fc9fbd43ddfe
Keivan <keyvan0082@gmail.com>
2022-09-04 20:44:31 +0430

    Animate install onlick copy

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,13 @@
 const installBox = document.querySelector(".install-box");
+const installAnimation = document.querySelector(".install-box-animation");
 installBox.onclick = () => {
-    const clipBoardText = installBox.textContent;
+    const clipBoardText = installBox.textContent.trim();
     navigator.clipboard.writeText(clipBoardText).catch();
+    animateCopyAction();
+};
+
+const animateCopyAction = () => {
+    if (installAnimation.classList.contains("animation-stats")) return;
+    installAnimation.classList.add("animation-stats");
+    setTimeout(() => installAnimation.classList.remove("animation-stats"), 500);
 };
