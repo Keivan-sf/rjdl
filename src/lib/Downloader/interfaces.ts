40e950c8ce136986d947b9a7c61c6f7ed88bd6a6
Keivan <keyvan0082@gmail.com>
2022-09-04 16:47:58 +0430

    Prettify Downloader interfaces

--- src/lib/Downloader/interfaces.ts
+++ src/lib/Downloader/interfaces.ts
@@ -5,10 +5,10 @@ export type DownloadLinks = { midQuality: string; highQuality: string };
  * @typedef DownloadLinks
  * @prop {string} midQuality
  * Middle quality download link
- * 
+ *
  * For musics and podcasts it's `256`
  * @prop {string} highQuality
  * high quality download link
- * 
+ *
  * For musics and podcasts it's `320`
  */
