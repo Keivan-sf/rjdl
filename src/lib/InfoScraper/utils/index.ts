15c26a9743f34aa83a31e94fdd74f9d5071518bd
Keivan-sf <keyvan0082@gmail.com>
2023-01-21 17:57:04 +0330

    Add `user-agent` to request headers
    This change comes from a field represented in `__NEXT_DATA__` element.
    The field is called `isBot` and it's a boolean. If we send a request
    with normal axios headers it'll become true but if we simply use a
    browser-like header it'll be false. Also the user-agent is represented
    in the element as well. So I assume it's it's better to override the
    default headers with a browser-like one

--- src/lib/InfoScraper/utils/index.ts
+++ src/lib/InfoScraper/utils/index.ts
@@ -3,7 +3,15 @@ import { JSDOM } from "jsdom";
 export * from "./trackScraper";
 export * from "./PageScraper";
 export const getSourceCodeDOMDocument = async (url: string) => {
-    const sourceCode = (await axios.get(url)).data;
+    const sourceCode = (
+        await axios.get(url, {
+            method: "GET",
+            headers: {
+                "user-agent":
+                    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
+            },
+        })
+    ).data;
     const dom = new JSDOM(sourceCode);
     return dom.window.document;
 };
