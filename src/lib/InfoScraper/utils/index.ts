78d3738082b3793bc3c46474de9a84071d6ca22c
Keivan-sf <keyvan0082@gmail.com>
2023-01-23 19:16:15 +0330

    Update e2e tests
    End to end tests have been updated to match the new Rj design

--- src/lib/InfoScraper/utils/index.ts
+++ src/lib/InfoScraper/utils/index.ts
@@ -1,6 +1,5 @@
 import axios from "axios";
 import { JSDOM } from "jsdom";
-export * from "./trackScraper";
 export * from "./PageScraper";
 export const getSourceCodeDOMDocument = async (url: string) => {
     const sourceCode = (
